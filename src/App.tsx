8e6770345f2e3b9a12ed5731d7d94cdd9648704f
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-03-01 20:47:26 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react"
-import words from './wordList.json'
-
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  
   return (
-   <h1>
-
-   </h1>
-  )
+    <div className="flex flex-col items-center gap-8 mx-auto">
+      <div className="text-3xl text-center">Lose Win</div>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
