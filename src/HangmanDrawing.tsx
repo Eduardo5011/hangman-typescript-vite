dbb9f3ae5e9cee7da137d07138dd7a3c0d220a56
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-03-02 20:27:10 -0500

    updated

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,6 +1,6 @@
 
 const HEAD = (
-  <div className="w-[50px] h-[50px]"/>
+  <div className="w-[60px] h-[60px] rounded-full border-[10px] border-solid border-black absolute top-[50px] right-[-25px]"/>
 )
 
 
@@ -8,6 +8,7 @@ const HEAD = (
 const HangmanDrawing = () => {
   return (
     <div className="relative">
+      {HEAD}
       <div className="absolute h-[50px] w-[10px] bg-black top-0 right-0" />
       <div className="h-[10px] w-[200px] bg-black ml-[120px] " />
       <div className="h-[400px] w-[10px] bg-black ml-[120px] " />
