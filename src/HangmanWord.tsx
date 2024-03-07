99156463796482022fb4719d95cb60b92b44f863
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-03-07 18:31:08 -0500

    updated

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,6 +1,14 @@
 const HangmanWord = () => {
+  const word = "test";
+  const guestLetters = ["t",]
   return (
-    <div>HangmanWord</div>
-  )
-}
-export default HangmanWord
+    <div className="uppercase flex gap-2 text-[6rem] font-bold font-mono">
+      {word.split("").map((letter, index) => (
+        <span className="border-b-[5px] border-black">
+          <span className={`${guestLetters.includes(letter) ? '' : 'invisible'}`}>{letter}</span>
+        </span>
+      ))}
+    </div>
+  );
+};
+export default HangmanWord;
